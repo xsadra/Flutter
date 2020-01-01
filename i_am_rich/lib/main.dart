a8e1aea7c62b6f4e99cc76e24fce9e42df087ce5
Sadra <31988724+xsadra@users.noreply.github.com>
2020-01-01 20:03:27 +0100

    Using Scaffold and Image to load a pic from URL

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello Sadra!'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://sample-videos.com/img/Sample-jpg-image-100kb.jpg'),
+          ),
+        ),
       ),
     ),
   );
