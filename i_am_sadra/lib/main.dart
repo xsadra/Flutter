54e5b82ceb09453bdff6d72a8bcc182f379f235e
Sadra <31988724+xsadra@users.noreply.github.com>
2020-01-02 00:46:27 +0100

    Create new Project called 'I am Sadra'

---
+++ i_am_sadra/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am Sadra'),
+        ),
+      ),
+    ));
