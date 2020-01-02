5bf86ea832feed74f691aef99bff351ee11064ff
Sadra <31988724+xsadra@users.noreply.github.com>
2020-01-02 17:26:26 +0100

    Made 'Contact Me' page

--- contact_me/lib/main.dart
+++ contact_me/lib/main.dart
@@ -7,12 +7,86 @@ class ContactMe extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 60,
                 backgroundImage: AssetImage('images/me.jpg'),
+              ),
+              Text(
+                'Sadra Babai',
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'JAVA - FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans Pro',
+                    letterSpacing: 2.5),
+              ),
+              SizedBox(
+                height: 20,
+                width: 180,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text('+43 66 07 599 688',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.teal.shade900,
+                    fontFamily: 'Source Sans Pro'
+                  ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text('info@sadra.at',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.teal.shade900,
+                    fontFamily: 'Source Sans Pro'
+                  ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.public ,
+                    color: Colors.teal,
+                  ),
+                  title: Text('sadra.at',
+                    style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro'
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
