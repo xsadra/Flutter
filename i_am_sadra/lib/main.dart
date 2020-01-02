507f8b7af5d41216d03206a35defd861afafe81f
Sadra <31988724+xsadra@users.noreply.github.com>
2020-01-02 01:09:04 +0100

    Do same job as 'I am Rich' on 'I am Sadra'

--- i_am_sadra/lib/main.dart
+++ i_am_sadra/lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('I am Sadra'),
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.yellow[100],
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[700],
+        title: Text('I am Sadra'),
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/img.png'),
         ),
       ),
-    ));
+    ),
+  ));
+}
