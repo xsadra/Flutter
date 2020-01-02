cc23562a798af749b371f0d67a12ea29127105e0
Sadra <31988724+xsadra@users.noreply.github.com>
2020-01-02 12:07:57 +0100

    Add avatar picture to 'Contact Me' page

--- contact_me/lib/main.dart
+++ contact_me/lib/main.dart
@@ -6,7 +6,18 @@ class ContactMe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('images/me.jpg'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
