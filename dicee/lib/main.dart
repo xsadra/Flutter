f51f6d3c5791f49bdd2faafa6cf4356e5651bbdf
Sadra <31988724+xsadra@users.noreply.github.com>
2020-01-07 11:56:45 +0100

    finish 'Dicee' project

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,3 +1,61 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Text('Dicee'),
+            backgroundColor: Colors.red,
+          ),
+          body: DicePage(),
+        ),
+      ),
+    );
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 6;
+  int rightDiceNumber = 6;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Left Button got pressed.');
+                changeDicesNumber();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print('Right Button got pressed.');
+                changeDicesNumber();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void changeDicesNumber() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+}
