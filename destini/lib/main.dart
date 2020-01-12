f1896a5cc0f5fd3a78181f1edf98e019973d99cf
Sadra <31988724+xsadra@users.noreply.github.com>
2020-01-12 13:59:53 +0100

    Use the storyBrain to get the text for choices

--- destini/lib/main.dart
+++ destini/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'story_brain.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
 void main() => runApp(Destini());
 
 class Destini extends StatelessWidget {
@@ -57,8 +55,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.red,
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    storyBrain.getChoiceOne(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
@@ -79,8 +76,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   color: Colors.blue,
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
+                    storyBrain.getChoiceTwo(),
                     style: TextStyle(
                       fontSize: 20.0,
                     ),
