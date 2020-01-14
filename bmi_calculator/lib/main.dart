5f8e4c773005f4258e06db781b3799cef52c2afe
Sadra <31988724+xsadra@users.noreply.github.com>
2020-01-14 16:02:21 +0100

    Change the Theme to 'DARK'

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
       home: InputPage(),
     );
   }
