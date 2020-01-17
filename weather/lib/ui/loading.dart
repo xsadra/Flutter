3638069892ae01f6f6e421bc2db3be7adf9da229
Sadra <31988724+xsadra@users.noreply.github.com>
2020-01-17 17:16:21 +0100

    Add 'Loading' UserInterface

---
+++ weather/lib/ui/loading.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(onPressed: (){},
+        child: Text('Get Location'),),
+      ),
+    );
+  }
+}
