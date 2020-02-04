5f929f7ab1b46f7fea293beee7202cb583ac194e
Sadra <31988724+xsadra@users.noreply.github.com>
2020-02-04 23:00:42 +0100

    extract the 'CryptoCard' widget

--- bitcoin/lib/price_screen.dart
+++ bitcoin/lib/price_screen.dart
@@ -118,3 +118,38 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 }
+
+class CryptoCard extends StatelessWidget {
+  final String cryptoCurrency;
+  final String value;
+  final String selectedCurrency;
+
+  const CryptoCard(
+      {Key key, this.cryptoCurrency, this.value, this.selectedCurrency})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $cryptoCurrency = $value $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
