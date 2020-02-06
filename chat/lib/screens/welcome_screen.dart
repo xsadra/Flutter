26e8db528e3dea9720f94f3736f757e1b434143d
Sadra <31988724+xsadra@users.noreply.github.com>
2020-02-06 11:50:25 +0100

    Navigate to Login Screen by Tap on Login Button

--- chat/lib/screens/welcome_screen.dart
+++ chat/lib/screens/welcome_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'login_screen.dart';
+import 'registration_screen.dart';
+
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';
 
@@ -44,7 +47,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 borderRadius: BorderRadius.circular(30.0),
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
