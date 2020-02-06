bebeede48ffc525dbd039ff4b24ae4d8bfabce2a
Sadra <31988724+xsadra@users.noreply.github.com>
2020-02-06 15:27:41 +0100

    Remove the dark-theme and change the button-text color to white

--- chat/lib/widgets/rounded_button.dart
+++ chat/lib/widgets/rounded_button.dart
@@ -24,6 +24,7 @@ class RoundedButton extends StatelessWidget {
           height: 42.0,
           child: Text(
             title,
+            style: TextStyle(color: Colors.white),
           ),
         ),
       ),
