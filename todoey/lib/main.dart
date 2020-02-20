ed2700923845db54bca729b760bd06ff5cf0f70a
Sadra <31988724+xsadra@users.noreply.github.com>
2020-02-21 00:04:50 +0100

    Add new Class called 'TaskScreen'

--- todoey/lib/main.dart
+++ todoey/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:todoey/screens/tasks_screen.dart';
 
 void main() => runApp(MainApp());
 
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: TasksScreen(),
+    );
   }
 }
+
