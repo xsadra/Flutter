350b448a751fddb888890a5fe990b0d1c5a1265f
Sadra <31988724+xsadra@users.noreply.github.com>
2020-02-21 21:48:36 +0100

    Add new field to 'TaskData' to store temp value

--- todoey/lib/models/task_data.dart
+++ todoey/lib/models/task_data.dart
@@ -1,7 +1,10 @@
+import 'dart:collection';
+
 import 'package:flutter/foundation.dart';
 import 'package:todoey/models/task.dart';
 
 class TaskData extends ChangeNotifier {
+  String tempValue;
 
   List<Task> tasks = [
     Task(name: 'first task'),
