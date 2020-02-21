068d97b8d60a99d1805a9dc37d7ad7d1278b47b6
Sadra <31988724+xsadra@users.noreply.github.com>
2020-02-21 19:50:33 +0100

    Change the state of 'TasksList' class to 'StatefulWidget'

--- todoey/lib/widgets/tasks_list.dart
+++ todoey/lib/widgets/tasks_list.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:todoey/models/task.dart';
 import 'package:todoey/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'first task'),
+    Task(name: 'second task'),
+    Task(name: 'third task'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
+        TaskTile(taskTitle: tasks[0].name,isChecked: false,),
+        TaskTile(taskTitle: tasks[1].name,isChecked: false,),
+        TaskTile(taskTitle: tasks[2].name,isChecked: false,),
       ],
     );
   }
