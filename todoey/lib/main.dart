b22299c04fd0749762f7061fa55abb6401f2ae8d
Sadra <31988724+xsadra@users.noreply.github.com>
2020-02-21 20:26:04 +0100

    Using 'ChangeNotifierProvider' to provide TaskData class in main class

--- todoey/lib/main.dart
+++ todoey/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey/models/task_data.dart';
 import 'package:todoey/screens/tasks_screen.dart';
 
 void main() => runApp(MainApp());
@@ -6,8 +8,11 @@ void main() => runApp(MainApp());
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TasksScreen(),
+    return ChangeNotifierProvider(
+      create: (context) =>TaskData(),
+      child: MaterialApp(
+        home: TasksScreen(),
+      ),
     );
   }
 }
