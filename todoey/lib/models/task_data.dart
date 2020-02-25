72c2b3296a21c64c74d37d5dae12a9d851647945
Sadra <31988724+xsadra@users.noreply.github.com>
2020-02-25 20:07:18 +0100

    Add 'saveTasks' method to save 'tasks' into SharedPreferences Add 'readTasks' method to read 'tasks' from SharedPreferences Add 'loadTasks' method to read 'tasks' and notify to all listeners

--- todoey/lib/models/task_data.dart
+++ todoey/lib/models/task_data.dart
@@ -33,4 +33,34 @@ class TaskData extends ChangeNotifier {
     _tasks.remove(task);
     notifyListeners();
   }
+
+  void loadTasks() async {
+    _tasks = await _readTasks() ?? [Task(name: 'My first task')];
+    notifyListeners();
+  }
+
+  void _saveTasks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> rawDataTasks = [];
+    _tasks.forEach((element) {
+      rawDataTasks.add('${element.name}||${element.isDone.toString()}');
+    });
+    await prefs.setStringList('data', rawDataTasks);
+  }
+
+  Future<List<Task>> _readTasks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> rawDataTasks = prefs.getStringList('data') ?? [];
+    List<Task> tasks = [];
+    rawDataTasks.forEach((element) {
+      List<String> rawElement = element.split('||');
+      tasks.add(
+        Task(
+          name: rawElement[0],
+          isDone: rawElement[1] == 'true',
+        ),
+      );
+    });
+    return tasks;
+  }
 }
